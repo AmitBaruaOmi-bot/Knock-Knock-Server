772ee2516a0b6d94cc5827c7233ecfbb70005406
AmitBaruaOmi <shunnoomi@gmail.com>
2024-11-17 19:52:39 +0000

    new

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../models/user.model');
+const user = require('../models/user.model.js');
 const { body, validationResult } = require('express-validator');
 
 router.use(express.json());

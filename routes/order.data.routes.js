697b6648ca26fc4b6b4df03b1120fa47a1ac6c9d
AmitBaruaOmi <shunnoomi@gmail.com>
2024-11-27 03:09:27 +0000

    new

--- routes/order.data.routes.js
+++ routes/order.data.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.model.js');
 const orderData = require('../models/order.data.model.js'); // Ensure path is correct
 
 router.post('/orderData', async (req, res) => {
